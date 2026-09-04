65aed58f60cb50d4f2b4b2125c9bc710e1bae159
Bogdan Cotinghiu <110598161+Bogdy70@users.noreply.github.com>
2026-09-04 22:42:11 +0300

    Moved Common.h under Header files

--- TinyPytorch/Common.h
+++ TinyPytorch/Common.h
@@ -1,12 +1,10 @@
 #pragma once
 
-// Common definitions shared between CPU and CUDA tensor headers.
 enum class DataType
 {
 	float32,
 	int32
 };
 
-// Forward declarations
 class Tensor;
 class CPUTensor;
